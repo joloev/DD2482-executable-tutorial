4a323762145bcaec3b19243000f5337d7365022b
aissata <aimaiga2@gmail.com>
2022-04-06 15:24:59 +0200

    simplified

--- server_devops/src/main.rs
+++ server_devops/src/main.rs
@@ -14,34 +14,18 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 4096];
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-
     if buffer.starts_with(get) {
         let contents = fs::read_to_string("hello.html").unwrap();
 
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
+        let response = format!("HTTP/1.1 200 OK\r\nContent-Length: \r\n{}", contents);
 
-        stream.write(response.as_bytes()).unwrap();
+        stream.write_all(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-
-        let response = format!(
-            "{}\r\nContent-Length: {}\r\n\r\n{}",
-            status_line,
-            contents.len(),
-            contents
-        );
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    } // some other request
+        todo!();
+    }
 }
